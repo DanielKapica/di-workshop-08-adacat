8d4d102861b5971c246c16ed1e421e142b8a32c8
Daniel Kapica <kapica6105@gmail.com>
2018-10-29 10:20:32 +0000

    Update AdaCat.js

--- AdaCat.js
+++ AdaCat.js
@@ -4,7 +4,36 @@ class AdaCat {
     this.owner = owner
     this.hunger = 5
     this.isSleeping = false
+    this.isFed = false
+    this.isPlaying = false
     this.size = 30
+    this.message = ""
+  }
+
+  getDescription() {
+    var messageLine
+    if (this.isSleeping) {
+      messageLine = 'Shh! ' + this.name + ' is sleeping.'
+    } else if (this.isFed) {
+      this.isFed = false
+      messageLine = this.name + ' just ate food.'
+    } else if (this.isPlaying){
+      this.isPlaying = false
+      messageLine = this.name + ' is playing.'
+    } else{
+      messageLine = this.name + ' is awake.'
+    }
+
+    var lines = [
+      this.name + ' is a cat. they belong to ' + this.owner + '.',
+      'their hunger level is ' + this.hunger + '/10.',
+
+      'they weigh ' + this.size + ' kilograms.',
+      'their health is ' + this.getHealth() + '/30.',
+      messageLine
+    ]
+
+    return lines.join('\n')
   }
 
   setHunger(newHunger) {
@@ -17,24 +46,6 @@ class AdaCat {
     this.hunger = newHunger
   }
 
-  getDescription() {
-    var sleepLine
-    if (this.isSleeping) {
-      sleepLine = 'Shh! ' + this.name + ' is sleeping.'
-    } else {
-      sleepLine = this.name + ' is awake.'
-    }
-    var lines = [
-      this.name + ' is a cat. they belong to ' + this.owner + '.',
-      'their hunger level is ' + this.hunger + '/10.',
-
-      'they weigh ' + this.size + ' tonnes.',
-      'their health is ' + this.getHealth() + '/30.',
-      sleepLine
-    ]
-
-    return lines.join('\n')
-  }
 
   feed() {
     var hunger = this.hunger - 1
@@ -44,6 +55,7 @@ class AdaCat {
     }
 
     this.setHunger(hunger)
+    return this.isFed = true
   }
 
   nap() {
@@ -60,6 +72,7 @@ class AdaCat {
       this.size = this.size - 1
     }
     this.setHunger(hunger)
+    this.isPlaying = true
   }
 
   getHealth() {
